5bf7351f753a780c766c4fff629825138c735d41
afterparty <char6447@yahoo.com>
2019-04-15 20:29:51 -0500

    final part of the tutorial, added white noise generation

--- whisper/src/lib.rs
+++ whisper/src/lib.rs
@@ -1,7 +1,11 @@
+
 #[macro_use]
 extern crate vst;
+extern crate rand;
 
-use vst::plugin::{Info, Plugin};
+use vst::plugin::{Info, Plugin, Category};
+use vst::buffer::AudioBuffer;
+use rand::random;
 
 #[derive(Default)]
 struct Whisper;
@@ -13,10 +17,19 @@ impl Plugin for Whisper {
             unique_id: 1337,
             inputs: 0,
             outputs: 2,
-            category: Category::Synth
+            category: Category::Synth,
             ..Default::default()
         }
     }
-}
 
+    fn process(&mut self, buffer: &mut AudioBuffer<f32>){
+        let (_, output_buffer) = buffer.split();
+
+        for output_channel in output_buffer.into_iter() {
+            for output_sample in output_channel {
+                *output_sample = (random::<f32>() - 0.5f32) * 2f32;
+            }
+        }
+    }
+}
 plugin_main!(Whisper);
